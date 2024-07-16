e028476f806662ccf377e27cb30a2e9655f835d9
Sara Vargas <157633346+SVA-BL00@users.noreply.github.com>
2024-07-15 18:09:28 -0600

    Computer shaders completed

--- Assets/Scripts/GPUGraph.cs
+++ Assets/Scripts/GPUGraph.cs
@@ -7,25 +7,40 @@ public class GPUGraph : MonoBehaviour {
 		positionsId = Shader.PropertyToID("_Positions"),
 		resolutionId = Shader.PropertyToID("_Resolution"),
 		stepId = Shader.PropertyToID("_Step"),
-		timeId = Shader.PropertyToID("_Time");
+		timeId = Shader.PropertyToID("_Time"),
+		transitionProgressId = Shader.PropertyToID("_TransitionProgress");
 
 	void UpdateFunctionOnGPU () {
 		float step = 2f / resolution;
 		computeShader.SetInt(resolutionId, resolution);
 		computeShader.SetFloat(stepId, step);
 		computeShader.SetFloat(timeId, Time.time);
-		computeShader.SetBuffer(0, positionsId, positionsBuffer);
+
+		if (transitioning) {
+			computeShader.SetFloat(
+				transitionProgressId,
+				Mathf.SmoothStep(0f, 1f, duration / transitionDuration)
+			);
+		}
+
+		var kernelIndex =
+			(int)function +
+			(int)(transitioning ? transitionFunction : function) *
+			FunctionLibrary.FunctionCount;
+		computeShader.SetBuffer(kernelIndex, positionsId, positionsBuffer);
+		
 		int groups = Mathf.CeilToInt(resolution / 8f);
-		computeShader.Dispatch(0, groups, groups, 1);
+		computeShader.Dispatch(kernelIndex, groups, groups, 1);
 
 		material.SetBuffer(positionsId, positionsBuffer);
 		material.SetFloat(stepId, step);
 		var bounds = new Bounds(Vector3.zero, Vector3.one * (2f + 2f / resolution));
 		Graphics.DrawMeshInstancedProcedural(
-			mesh, 0, material, bounds, positionsBuffer.count
+			mesh, 0, material, bounds, resolution * resolution
 		);
 	}
-	[SerializeField, Range(10, 200)] int resolution = 10;
+	const int maxResolution = 1000;
+	[SerializeField, Range(10, maxResolution)] int resolution = 10;
 	[SerializeField] FunctionLibrary.FunctionName function;
 	public enum TransitionMode { Cycle, Random }
 	[SerializeField] TransitionMode transitionMode;
@@ -41,7 +56,7 @@ public class GPUGraph : MonoBehaviour {
 	ComputeBuffer positionsBuffer;
 
 	void OnEnable () {
-		positionsBuffer = new ComputeBuffer(resolution * resolution, 3 * 4);
+		positionsBuffer = new ComputeBuffer(maxResolution * maxResolution, 3 * 4);
 	}
 	void OnDisable () {
 		positionsBuffer.Release();
